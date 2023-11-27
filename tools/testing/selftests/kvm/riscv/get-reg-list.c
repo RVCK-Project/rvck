46261602829d30949270e3ee6c57cf18cb8dfbc0
Anup Patel <apatel@ventanamicro.com>
2023-11-27 18:47:23 +0530

    KVM: riscv: selftests: Add Zbc extension to get-reg-list test
    mainline inclusion
    from Linux 6.8-rc4
    commit ac396141308d07a9534c5a7f1f7c80cb95e35b20
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/357
    
    [Because the previously submitted patch "KVM: riscv: selftests: Add Sscofpmf to get-reg-list
    test" (PR 218[1]) incorrectly included portions of this patch, the integration of this patch as
    submitted in this commit is incomplete relative to the upstream version.]
    
    [1]: https://github.com/RVCK-Project/rvck/pull/218
    --------------------------------
    
    The KVM RISC-V allows Zbc extension for Guest/VM so let us add
    this extension to get-reg-list test.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Signed-off-by: Anup Patel <anup@brainfault.org>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>

--- tools/testing/selftests/kvm/riscv/get-reg-list.c
+++ tools/testing/selftests/kvm/riscv/get-reg-list.c
@@ -435,6 +435,7 @@ static const char *isa_ext_single_id_to_str(__u64 reg_off)
 		KVM_ISA_EXT_ARR(SVPBMT),
 		KVM_ISA_EXT_ARR(ZBA),
 		KVM_ISA_EXT_ARR(ZBB),
+		KVM_ISA_EXT_ARR(ZBC),
 		KVM_ISA_EXT_ARR(ZBS),
 		KVM_ISA_EXT_ARR(ZCLSD),
 		KVM_ISA_EXT_ARR(ZICBOM),
@@ -947,6 +948,7 @@ KVM_ISA_EXT_SIMPLE_CONFIG(svnapot, SVNAPOT);
 KVM_ISA_EXT_SIMPLE_CONFIG(svpbmt, SVPBMT);
 KVM_ISA_EXT_SIMPLE_CONFIG(zba, ZBA);
 KVM_ISA_EXT_SIMPLE_CONFIG(zbb, ZBB);
+KVM_ISA_EXT_SIMPLE_CONFIG(zbc, ZBC);
 KVM_ISA_EXT_SIMPLE_CONFIG(zbs, ZBS);
 KVM_ISA_EXT_SIMPLE_CONFIG(zclsd, ZCLSD);
 KVM_ISA_EXT_SUBLIST_CONFIG(zicbom, ZICBOM);
@@ -980,6 +982,7 @@ struct vcpu_reg_list *vcpu_configs[] = {
 	&config_svpbmt,
 	&config_zba,
 	&config_zbb,
+	&config_zbc,
 	&config_zbs,
 	&config_zclsd,
 	&config_zicbom,
