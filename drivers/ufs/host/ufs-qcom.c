21965770777f2e7822524b8013c7e9505cf17792
Thomas Gleixner <tglx@linutronix.de>
2024-01-27 21:47:34 +0530

    irqchip: Convert all platform MSI users to the new API
    mainline inclusion
    commit 14fd06c776b5289a43c91cdc64bac3bdbc7b397e
    category: cleanup
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/1
    
    --------------------------------
    
    Switch all the users of the platform MSI domain over to invoke the new
    interfaces which branch to the original platform MSI functions when the
    irqdomain associated to the caller device does not yet provide MSI parent
    functionality.
    
    No functional change.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20240127161753.114685-7-apatel@ventanamicro.com
    Signed-off-by: Hangfan Li <lihangfan@iscas.ac.cn>

--- drivers/ufs/host/ufs-qcom.c
+++ drivers/ufs/host/ufs-qcom.c
@@ -1812,8 +1812,8 @@ static int ufs_qcom_config_esi(struct ufs_hba *hba)
 	 * 2. Poll queues do not need ESI.
 	 */
 	nr_irqs = hba->nr_hw_queues - hba->nr_queues[HCTX_TYPE_POLL];
-	ret = platform_msi_domain_alloc_irqs(hba->dev, nr_irqs,
-					     ufs_qcom_write_msi_msg);
+	ret = platform_device_msi_init_and_alloc_irqs(hba->dev, nr_irqs,
+						      ufs_qcom_write_msi_msg);
 	if (ret) {
 		dev_err(hba->dev, "Failed to request Platform MSI %d\n", ret);
 		goto out;
@@ -1842,7 +1842,7 @@ static int ufs_qcom_config_esi(struct ufs_hba *hba)
 			devm_free_irq(hba->dev, desc->irq, hba);
 		}
 		msi_unlock_descs(hba->dev);
-		platform_msi_domain_free_irqs(hba->dev);
+		platform_device_msi_free_irqs_all(hba->dev);
 	} else {
 		if (host->hw_ver.major == 6 && host->hw_ver.minor == 0 &&
 		    host->hw_ver.step == 0) {
@@ -1922,7 +1922,8 @@ static void ufs_qcom_remove(struct platform_device *pdev)
 
 	ufshcd_pltfrm_remove(pdev);
 	if (host->esi_enabled)
-		platform_msi_domain_free_irqs(hba->dev);
+		platform_device_msi_free_irqs_all(hba->dev);
+	return 0;
 }
 
 static const struct of_device_id ufs_qcom_of_match[] __maybe_unused = {
