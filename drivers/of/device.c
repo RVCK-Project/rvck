44f776d9bf8290e8ecee7467ad9bbbfddb87cca9
Robin Murphy <robin.murphy@arm.com>
2024-04-19 17:54:40 +0100

    OF: Retire dma-ranges mask workaround
    mainline inclusion
    from Linux 6.10-rc1
    commit 0c3457926e7e65710f32e02920c7d423417c93a2
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/286
    
    --------------------------------
    
    The fixup adding 1 to the dma-ranges size may have been for the benefit
    of some early AMD Seattle DTs, or may have merely been a just-in-case,
    but either way anyone who might have deserved to get the message has
    hopefully seen the warning in the 9 years we've had it there. The modern
    dma_range_map mechanism should happily handle odd-sized ranges with no
    ill effect, so there's little need to care anyway now. Clean it up.
    
    Acked-by: Rob Herring <robh@kernel.org>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>

--- drivers/of/device.c
+++ drivers/of/device.c
@@ -134,21 +134,6 @@ int of_dma_configure_id(struct device *dev, struct device_node *np,
 		}
 		size = dma_end - dma_start;
 
-		/*
-		 * Add a work around to treat the size as mask + 1 in case
-		 * it is defined in DT as a mask.
-		 */
-		if (size & 1) {
-			dev_warn(dev, "Invalid size 0x%llx for dma-range(s)\n",
-				 size);
-			size = size + 1;
-		}
-
-		if (!size) {
-			dev_err(dev, "Adjusted size 0x%llx invalid\n", size);
-			kfree(map);
-			return -EINVAL;
-		}
 		set_map = true;
 	}
 skip_map:
