259a033dbb579aa7db42973725778e9e5463d96a
Robin Murphy <robin.murphy@arm.com>
2024-04-19 17:54:41 +0100

    OF: Simplify DMA range calculations
    mainline inclusion
    from Linux 6.10-rc1
    commit ba503cf41c90e173f9421f7882d84b228bada97a
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/286
    
    --------------------------------
    
    Juggling start, end, and size values for a range is somewhat redundant
    and a little hard to follow. Consolidate down to just using inclusive
    start and end, which saves us worrying about size overflows for full
    64-bit ranges (note that passing a potentially-overflowed value through
    to arch_setup_dma_ops() is benign for all current implementations, and
    this is working towards removing that anyway).
    
    Acked-by: Rob Herring <robh@kernel.org>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>

--- drivers/of/device.c
+++ drivers/of/device.c
@@ -96,7 +96,7 @@ int of_dma_configure_id(struct device *dev, struct device_node *np,
 	const struct bus_dma_region *map = NULL;
 	struct device_node *bus_np;
 	u64 dma_start = 0;
-	u64 mask, end, size = 0;
+	u64 mask, end = 0;
 	bool coherent, set_map = false;
 	int ret;
 
@@ -122,17 +122,15 @@ int of_dma_configure_id(struct device *dev, struct device_node *np,
 			return ret == -ENODEV ? 0 : ret;
 	} else {
 		const struct bus_dma_region *r = map;
-		u64 dma_end = 0;
 
 		/* Determine the overall bounds of all DMA regions */
 		for (dma_start = ~0; r->size; r++) {
 			/* Take lower and upper limits */
 			if (r->dma_start < dma_start)
 				dma_start = r->dma_start;
-			if (r->dma_start + r->size > dma_end)
-				dma_end = r->dma_start + r->size;
+			if (r->dma_start + r->size > end)
+				end = r->dma_start + r->size;
 		}
-		size = dma_end - dma_start;
 
 		set_map = true;
 	}
@@ -148,16 +146,15 @@ skip_map:
 		dev->dma_mask = &dev->coherent_dma_mask;
 	}
 
-	if (!size && dev->coherent_dma_mask)
-		size = max(dev->coherent_dma_mask, dev->coherent_dma_mask + 1);
-	else if (!size)
-		size = 1ULL << 32;
+	if (!end && dev->coherent_dma_mask)
+		end = dev->coherent_dma_mask;
+	else if (!end)
+		end = (1ULL << 32) - 1;
 
 	/*
 	 * Limit coherent and dma mask based on size and default mask
 	 * set by the driver.
 	 */
-	end = dma_start + size - 1;
 	mask = DMA_BIT_MASK(ilog2(end) + 1);
 	dev->coherent_dma_mask &= mask;
 	*dev->dma_mask &= mask;
@@ -183,7 +180,7 @@ skip_map:
 	dev_dbg(dev, "device is%sbehind an iommu\n",
 		!ret ? " " : " not ");
 
-	arch_setup_dma_ops(dev, dma_start, size, coherent);
+	arch_setup_dma_ops(dev, dma_start, end - dma_start + 1, coherent);
 
 	if (ret)
 		of_dma_set_restricted_buffer(dev, np);
