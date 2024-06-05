9142fcaed0c64bb67e2fc54931e2e676c7d04c18
Björn Töpel <bjorn@rivosinc.com>
2024-06-05 13:40:44 +0200

    riscv: mm: Properly forward vmemmap_populate() altmap parameter
    mainline inclusion
    from mainline-Linux 6.10-rc6
    commit e3ecf2fdc8f39a898f9e06481e935b460a097e10
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/219
    
    --------------------------------
    
    Make sure that the altmap parameter is properly passed on to
    vmemmap_populate_hugepages().
    
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://lore.kernel.org/r/20240605114100.315918-2-bjorn@kernel.org
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>

--- arch/riscv/mm/init.c
+++ arch/riscv/mm/init.c
@@ -1752,7 +1752,7 @@ int __meminit vmemmap_populate(unsigned long start, unsigned long end, int node,
 	 * memory hotplug, we are not able to update all the page tables with
 	 * the new PMDs.
 	 */
-	return vmemmap_populate_hugepages(start, end, node, NULL);
+	return vmemmap_populate_hugepages(start, end, node, altmap);
 }
 #endif
 
