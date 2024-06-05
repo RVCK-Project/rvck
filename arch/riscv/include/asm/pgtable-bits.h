c42a387c55a53389fea99401794358ef48c4e09f
Björn Töpel <bjorn@rivosinc.com>
2024-06-05 13:40:53 +0200

    riscv: mm: Add support for ZONE_DEVICE
    mainline inclusion
    from mainline-Linux 6.10-rc6
    commit 216e04bf1e4d933fe8338e486a9dff93c208601e
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/219
    
    --------------------------------
    
    ZONE_DEVICE pages need DEVMAP PTEs support to function
    (ARCH_HAS_PTE_DEVMAP). Claim another RSW (reserved for software) bit
    in the PTE for DEVMAP mark, add the corresponding helpers, and enable
    ARCH_HAS_PTE_DEVMAP for riscv64.
    
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://lore.kernel.org/r/20240605114100.315918-11-bjorn@kernel.org
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>

--- arch/riscv/include/asm/pgtable-bits.h
+++ arch/riscv/include/asm/pgtable-bits.h
@@ -17,6 +17,7 @@
 #define _PAGE_ACCESSED  (1 << 6)    /* Set by hardware on any access */
 #define _PAGE_DIRTY     (1 << 7)    /* Set by hardware on any write */
 #define _PAGE_SOFT      (1 << 8)    /* Reserved for software */
+#define _PAGE_DEVMAP    (1 << 9)    /* RSW, devmap */
 
 #define _PAGE_SPECIAL   _PAGE_SOFT
 #define _PAGE_TABLE     _PAGE_PRESENT
