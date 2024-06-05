c80aa643f570f20d6a86fc5ca7f0aec1b8745fb6
Björn Töpel <bjorn@rivosinc.com>
2024-06-05 13:40:46 +0200

    riscv: mm: Change attribute from __init to __meminit for page functions
    mainline inclusion
    from mainline-Linux 6.10-rc6
    commit fe122b89da670be155f3474c0cb28af2fbcd2ecc
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/219
    
    --------------------------------
    
    Prepare for memory hotplugging support by changing from __init to
    __meminit for the page table functions that are used by the upcoming
    architecture specific callbacks.
    
    Changing the __init attribute to __meminit, avoids that the functions
    are removed after init. The __meminit attribute makes sure the
    functions are kept in the kernel text post init, but only if memory
    hotplugging is enabled for the build.
    
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Reviewed-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Oscar Salvador <osalvador@suse.de>
    Signed-off-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://lore.kernel.org/r/20240605114100.315918-4-bjorn@kernel.org
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>

--- arch/riscv/include/asm/mmu.h
+++ arch/riscv/include/asm/mmu.h
@@ -26,8 +26,8 @@ typedef struct {
 #endif
 } mm_context_t;
 
-void __init create_pgd_mapping(pgd_t *pgdp, uintptr_t va, phys_addr_t pa,
-			       phys_addr_t sz, pgprot_t prot);
+void __meminit create_pgd_mapping(pgd_t *pgdp, uintptr_t va, phys_addr_t pa, phys_addr_t sz,
+				  pgprot_t prot);
 #endif /* __ASSEMBLY__ */
 
 #endif /* _ASM_RISCV_MMU_H */
