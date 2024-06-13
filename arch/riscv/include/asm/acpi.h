7282ee8bda9b07b2159c72c4905820ddb7b3c848
Haibo Xu <haibo1.xu@intel.com>
2024-06-13 16:54:33 +0800

    ACPI: RISCV: Add NUMA support based on SRAT and SLIT
    mainline inclusion
    from mainline-v6.11-rc1
    commit eabd9db64ea8ba64d2a0b1d70da38e1a95dcd08b
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/37
    
    --------------------------------
    
    Add acpi_numa.c file to enable parse NUMA information from
    ACPI SRAT and SLIT tables. SRAT table provide CPUs(Hart) and
    memory nodes to proximity domain mapping, while SLIT table
    provide the distance metrics between proximity domains.
    
    Signed-off-by: Haibo Xu <haibo1.xu@intel.com>
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- arch/riscv/include/asm/acpi.h
+++ arch/riscv/include/asm/acpi.h
@@ -61,11 +61,14 @@ static inline void arch_fix_phys_package_id(int num, u32 slot) { }
 
 void acpi_init_rintc_map(void);
 struct acpi_madt_rintc *acpi_cpu_get_madt_rintc(int cpu);
-u32 get_acpi_id_for_cpu(int cpu);
+static inline u32 get_acpi_id_for_cpu(int cpu)
+{
+	return acpi_cpu_get_madt_rintc(cpu)->uid;
+}
+
 int acpi_get_riscv_isa(struct acpi_table_header *table,
 		       unsigned int cpu, const char **isa);
 
-static inline int acpi_numa_get_nid(unsigned int cpu) { return NUMA_NO_NODE; }
 #else
 static inline void acpi_init_rintc_map(void) { }
 static inline struct acpi_madt_rintc *acpi_cpu_get_madt_rintc(int cpu)
@@ -80,5 +83,13 @@ static inline int acpi_get_riscv_isa(struct acpi_table_header *table,
 }
 
 #endif /* CONFIG_ACPI */
+
+#ifdef CONFIG_ACPI_NUMA
+int acpi_numa_get_nid(unsigned int cpu);
+void acpi_map_cpus_to_nodes(void);
+#else
+static inline int acpi_numa_get_nid(unsigned int cpu) { return NUMA_NO_NODE; }
+static inline void acpi_map_cpus_to_nodes(void) { }
+#endif /* CONFIG_ACPI_NUMA */
 
 #endif /*_ASM_ACPI_H*/
