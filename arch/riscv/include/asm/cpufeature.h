b9077f7275e8dbcf9b081467a6b0bae4f31106dd
Charlie Jenkins <charlie@rivosinc.com>
2024-07-19 09:15:18 -0700

    riscv: Extend cpufeature.c to detect vendor extensions
    mainline inclusion
    from mainline-v6.13
    commit 23c996fc2bc1978a02c64eddb90b4ab5d309c8df
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/36
    
    --------------------------------
    
    Instead of grouping all vendor extensions into the same riscv_isa_ext
    that standard instructions use, create a struct
    "riscv_isa_vendor_ext_data_list" that allows each vendor to maintain
    their vendor extensions independently of the standard extensions.
    xandespmu is currently the only vendor extension so that is the only
    extension that is affected by this change.
    
    An additional benefit of this is that the extensions of each vendor can
    be conditionally enabled. A config RISCV_ISA_VENDOR_EXT_ANDES has been
    added to allow for that.
    
    Signed-off-by: Charlie Jenkins <charlie@rivosinc.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>
    Reviewed-by: Andy Chiu <andy.chiu@sifive.com>
    Tested-by: Yu Chien Peter Lin <peterlin@andestech.com>
    Reviewed-by: Yu Chien Peter Lin <peterlin@andestech.com>
    Link: https://lore.kernel.org/r/20240719-support_vendor_extensions-v3-1-0af7587bbec0@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Weihao Li <ieiao@outlook.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- arch/riscv/include/asm/cpufeature.h
+++ arch/riscv/include/asm/cpufeature.h
@@ -36,6 +36,31 @@ extern struct riscv_isainfo hart_isa[NR_CPUS];
 void check_unaligned_access(int cpu);
 void riscv_user_isa_enable(void);
 
+#define _RISCV_ISA_EXT_DATA(_name, _id, _subset_exts, _subset_exts_size, _validate) {	\
+	.name = #_name,									\
+	.property = #_name,								\
+	.id = _id,									\
+	.subset_ext_ids = _subset_exts,							\
+	.subset_ext_size = _subset_exts_size,						\
+	.validate = _validate								\
+}
+
+#define __RISCV_ISA_EXT_DATA(_name, _id) _RISCV_ISA_EXT_DATA(_name, _id, NULL, 0, NULL)
+
+#define __RISCV_ISA_EXT_DATA_VALIDATE(_name, _id, _validate) \
+			_RISCV_ISA_EXT_DATA(_name, _id, NULL, 0, _validate)
+
+/* Used to declare pure "lasso" extension (Zk for instance) */
+#define __RISCV_ISA_EXT_BUNDLE(_name, _bundled_exts) \
+	_RISCV_ISA_EXT_DATA(_name, RISCV_ISA_EXT_INVALID, _bundled_exts, \
+			    ARRAY_SIZE(_bundled_exts), NULL)
+
+/* Used to declare extensions that are a superset of other extensions (Zvbb for instance) */
+#define __RISCV_ISA_EXT_SUPERSET(_name, _id, _sub_exts) \
+	_RISCV_ISA_EXT_DATA(_name, _id, _sub_exts, ARRAY_SIZE(_sub_exts), NULL)
+#define __RISCV_ISA_EXT_SUPERSET_VALIDATE(_name, _id, _sub_exts, _validate) \
+	_RISCV_ISA_EXT_DATA(_name, _id, _sub_exts, ARRAY_SIZE(_sub_exts), _validate)
+
 unsigned long riscv_get_elf_hwcap(void);
 
 struct riscv_isa_ext_data {
