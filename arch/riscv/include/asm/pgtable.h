2aae953a0678c1f23d01556b21c6f178ae71cd21
Yong-Xuan Wang <yongxuan.wang@sifive.com>
2024-07-26 16:49:26 +0800

    RISC-V: Add Svade and Svadu Extensions Support
    mainline inclusion
    from mainline-v6.13
    commit 94a7734d0967e89fac5be1fd5115f5194e4a4017
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/36
    
    --------------------------------
    
    Svade and Svadu extensions represent two schemes for managing the PTE A/D
    bits. When the PTE A/D bits need to be set, Svade extension intdicates
    that a related page fault will be raised. In contrast, the Svadu extension
    supports hardware updating of PTE A/D bits. Since the Svade extension is
    mandatory and the Svadu extension is optional in RVA23 profile, by default
    the M-mode firmware will enable the Svadu extension in the menvcfg CSR
    when only Svadu is present in DT.
    
    This patch detects Svade and Svadu extensions from DT and adds
    arch_has_hw_pte_young() to enable optimization in MGLRU and
    __wp_page_copy_user() when we have the PTE A/D bits hardware updating
    support.
    
    Co-developed-by: Jinyu Tang <tjytimi@163.com>
    Signed-off-by: Jinyu Tang <tjytimi@163.com>
    Signed-off-by: Yong-Xuan Wang <yongxuan.wang@sifive.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Acked-by: Palmer Dabbelt <palmer@rivosinc.com>
    Link: https://lore.kernel.org/r/20240726084931.28924-2-yongxuan.wang@sifive.com
    Signed-off-by: Anup Patel <anup@brainfault.org>
    Signed-off-by: Weihao Li <ieiao@outlook.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- arch/riscv/include/asm/pgtable.h
+++ arch/riscv/include/asm/pgtable.h
@@ -130,6 +130,7 @@
 #include <asm/tlbflush.h>
 #include <linux/mm_types.h>
 #include <asm/compat.h>
+#include <asm/cpufeature.h>
 
 #define __page_val_to_pfn(_val)  (((_val) & _PAGE_PFN_MASK) >> _PAGE_PFN_SHIFT)
 
@@ -305,7 +306,6 @@ static inline pte_t pud_pte(pud_t pud)
 }
 
 #ifdef CONFIG_RISCV_ISA_SVNAPOT
-#include <asm/cpufeature.h>
 
 static __always_inline bool has_svnapot(void)
 {
@@ -640,6 +640,17 @@ static inline pgprot_t pgprot_writecombine(pgprot_t _prot)
 }
 
 #define pgprot_dmacoherent pgprot_writecombine
+
+/*
+ * Both Svade and Svadu control the hardware behavior when the PTE A/D bits need to be set. By
+ * default the M-mode firmware enables the hardware updating scheme when only Svadu is present in
+ * DT.
+ */
+#define arch_has_hw_pte_young arch_has_hw_pte_young
+static inline bool arch_has_hw_pte_young(void)
+{
+	return riscv_has_extension_unlikely(RISCV_ISA_EXT_SVADU);
+}
 
 /*
  * THP functions
