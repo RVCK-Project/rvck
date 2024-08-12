81ebaf291f62576d1f1e55977df0012deb9979ed
Sunil V L <sunilvl@ventanamicro.com>
2024-08-12 06:29:20 +0530

    ACPI: pci_link: Clear the dependencies after probe
    mainline inclusion
    from mainline-v6.12-rc1
    commit 2cb9155d116c4d9dd7610139a876d123b75924ef
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/40
    
    --------------------------------
    
    RISC-V platforms need to use dependencies between PCI host bridge, Link
    devices and the interrupt controllers to ensure probe order. The
    dependency is like below.
    
    Interrupt controller <-- Link Device <-- PCI Host bridge.
    
    If there is no dependency between Link device and PCI Host Bridge,
    then PCI devices may be probed prior to Link devices.  If a PCI
    device is probed before its Link device, we won't be able to find
    its INTx mapping.
    
    So, add the link device's HID to dependency honor list and clear the
    dependency after probe is done so that the dependent devices are
    unblocked to probe.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Tested-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://patch.msgid.link/20240812005929.113499-9-sunilvl@ventanamicro.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: haijiao.liu <haijiao.liu@sophgo.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -796,6 +796,7 @@ static const char * const acpi_honor_dep_ids[] = {
 	"INTC100A", /* IVSC (RPL) driver must be loaded to allow i2c access to camera sensors */
 	"RSCV0001", /* RISC-V PLIC */
 	"RSCV0002", /* RISC-V APLIC */
+	"PNP0C0F",  /* PCI Link Device */
 	NULL
 };
 
