d0ba3a68b8bb00a3671aef8d85445f44938ea0b2
Sunil V L <sunilvl@ventanamicro.com>
2024-08-12 06:29:16 +0530

    ACPI: scan: Refactor dependency creation
    mainline inclusion
    from mainline-v6.12-rc1
    commit 76d749c58f4c6dd333cb8ea515373f7c8df96e78
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/40
    
    --------------------------------
    
    Some architectures like RISC-V will use implicit dependencies like GSI
    map to create dependencies between interrupt controller and devices. To
    support doing that, the function which creates the dependency, is
    refactored bit and made public so that dependency can be added from
    outside of scan.c as well.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>
    Tested-by: Björn Töpel <bjorn@rivosinc.com>
    Link: https://patch.msgid.link/20240812005929.113499-5-sunilvl@ventanamicro.com
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [Resolve merge conflicts in include/acpi/acpi_bus.h]
    Signed-off-by: haijiao.liu <haijiao.liu@sophgo.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- include/acpi/acpi_bus.h
+++ include/acpi/acpi_bus.h
@@ -838,6 +838,8 @@ static inline void acpi_put_acpi_dev(struct acpi_device *adev)
 {
 	acpi_dev_put(adev);
 }
+
+int acpi_scan_add_dep(acpi_handle handle, struct acpi_handle_list *dep_devices);
 #else	/* CONFIG_ACPI */
 
 static inline int register_acpi_bus_type(void *bus) { return 0; }
