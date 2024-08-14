a10e364cfab25fd6460e9cb9963be06149187fbf
Samuel Holland <samuel.holland@sifive.com>
2024-08-14 01:10:54 -0700

    riscv: Enable cbo.zero only when all harts support Zicboz
    mainline inclusion
    from mainline-v6.13
    commit 1b57747e978f920fb2affd1952ed913276019115
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/36
    
    --------------------------------
    
    Currently, we enable cbo.zero for usermode on each hart that supports
    the Zicboz extension. This means that the [ms]envcfg CSR value may
    differ between harts. Other features, such as pointer masking and CFI,
    require setting [ms]envcfg bits on a per-thread basis. The combination
    of these two adds quite some complexity and overhead to context
    switching, as we would need to maintain two separate masks for the
    per-hart and per-thread bits. Andrew Jones, who originally added Zicboz
    support, writes[1][2]:
    
      I've approached Zicboz the same way I would approach all
      extensions, which is to be per-hart. I'm not currently aware of
      a platform that is / will be composed of harts where some have
      Zicboz and others don't, but there's nothing stopping a platform
      like that from being built.
    
      So, how about we add code that confirms Zicboz is on all harts.
      If any hart does not have it, then we complain loudly and disable
      it on all the other harts. If it was just a hardware description
      bug, then it'll get fixed. If there's actually a platform which
      doesn't have Zicboz on all harts, then, when the issue is reported,
      we can decide to not support it, support it with defconfig, or
      support it under a Kconfig guard which must be enabled by the user.
    
    Let's follow his suggested solution and require the extension to be
    available on all harts, so the envcfg CSR value does not need to change
    when a thread migrates between harts. Since we are doing this for all
    extensions with fields in envcfg, the CSR itself only needs to be saved/
    restored when it is present on all harts.
    
    This should not be a regression as no known hardware has asymmetric
    Zicboz support, but if anyone reports seeing the warning, we will
    re-evaluate our solution.
    
    Link: https://lore.kernel.org/linux-riscv/20240322-168f191eeb8479b2ea169a5e@orel/ [1]
    Link: https://lore.kernel.org/linux-riscv/20240323-28943722feb57a41fb0ff488@orel/ [2]
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Reviewed-by: Conor Dooley <conor.dooley@microchip.com>
    Reviewed-by: Deepak Gupta <debug@rivosinc.com>
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
    Reviewed-by: Charlie Jenkins <charlie@rivosinc.com>
    Tested-by: Charlie Jenkins <charlie@rivosinc.com>
    Link: https://lore.kernel.org/r/20240814081126.956287-2-samuel.holland@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Weihao Li <ieiao@outlook.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- arch/riscv/kernel/cpufeature.c
+++ arch/riscv/kernel/cpufeature.c
@@ -33,6 +33,8 @@
 #define MISALIGNED_BUFFER_SIZE 0x4000
 #define MISALIGNED_COPY_SIZE ((MISALIGNED_BUFFER_SIZE / 2) - 0x80)
 
+static bool any_cpu_has_zicboz;
+
 unsigned long elf_hwcap __read_mostly;
 
 /* Host ISA bitmap */
@@ -106,6 +108,7 @@ static int riscv_ext_zicboz_validate(const struct riscv_isa_ext_data *data,
 		pr_err("Zicboz disabled as cboz-block-size present, but is not a power-of-2\n");
 		return -EINVAL;
 	}
+	any_cpu_has_zicboz = true;
 	return 0;
 }
 
@@ -1029,8 +1032,10 @@ arch_initcall(check_unaligned_access_boot_cpu);
 
 void riscv_user_isa_enable(void)
 {
-	if (riscv_cpu_has_extension_unlikely(smp_processor_id(), RISCV_ISA_EXT_ZICBOZ))
+	if (riscv_has_extension_unlikely(RISCV_ISA_EXT_ZICBOZ))
 		csr_set(CSR_ENVCFG, ENVCFG_CBZE);
+	else if (any_cpu_has_zicboz)
+		pr_warn_once("Zicboz disabled as it is unavailable on some harts\n");
 }
 
 #ifdef CONFIG_RISCV_ALTERNATIVE
