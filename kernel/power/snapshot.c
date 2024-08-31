be05e6e9bca99b3252eee24385d09d2ecab8abfb
xianbing Zhu <xianbing.zhu@linux.alibaba.com>
2024-09-01 01:31:49 +0800

    hibernate: speedup image crc caculation by multi-threads
    This commit introduce a speedup method for crc caculated
    in each fragment , and saved in its lzo header.
    
    This helps make std load image speed faster, about
    50% rate increase:
    
    Change-Id: Ib2777e0a0ea275306fd57d77d3e09a852c779ab8
    
    ---- with this commit ----
    PM: hibernation: Read 512588 kbytes in 1.71 seconds (299.75 MB/s)
    ---- before this commit ----
    PM: hibernation: Read 514420 kbytes in 2.87 seconds (179.24 MB/s)
    
    v2: crc runs right after decompress makes faster than
    on its own crc thread.
    
    Signed-off-by: xianbing Zhu <xianbing.zhu@linux.alibaba.com>
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- kernel/power/snapshot.c
+++ kernel/power/snapshot.c
@@ -2872,6 +2872,7 @@ int snapshot_write_next(struct snapshot_handle *handle)
 {
 	static struct chain_allocator ca;
 	int error = 0;
+	ktime_t start;
 
 next:
 	/* Check if we have already loaded the entire image */
@@ -2911,10 +2912,12 @@ next:
 			return error;
 
 		if (handle->cur == nr_meta_pages + 1) {
+			start = ktime_get();
 			error = prepare_image(&orig_bm, &copy_bm, &zero_bm);
 			if (error)
 				return error;
 
+			pr_info("prepare image took %lldms\n",ktime_to_ms( ktime_sub(ktime_get(),start)) );
 			chain_init(&ca, GFP_ATOMIC, PG_SAFE);
 			memory_bm_position_reset(&orig_bm);
 			memory_bm_position_reset(&zero_bm);
