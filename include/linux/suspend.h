e745d40d24572527454086602a98182dd20ff602
xianbing Zhu <xianbing.zhu@linux.alibaba.com>
2024-09-01 01:11:25 +0800

    hibernate: add interface for driver to mark no_save region
    Add this two function hibernate_register_nosave_region and
    hibernate_remove_nosave_region to register or remove nosave
    region for driver.
    This help reduce size of hibernate image.
    
    Signed-off-by: xianbing Zhu <xianbing.zhu@linux.alibaba.com>
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- include/linux/suspend.h
+++ include/linux/suspend.h
@@ -443,6 +443,9 @@ extern asmlinkage int swsusp_arch_suspend(void);
 extern asmlinkage int swsusp_arch_resume(void);
 
 extern u32 swsusp_hardware_signature;
+extern int hibernate_register_nosave_region(unsigned long start_pfn, unsigned long end_pfn);
+extern int hibernate_remove_nosave_region(unsigned long start_pfn, unsigned long end_pfn);
+
 extern void hibernation_set_ops(const struct platform_hibernation_ops *ops);
 extern int hibernate(void);
 extern bool system_entering_hibernation(void);
@@ -461,6 +464,13 @@ static inline void register_nosave_region(unsigned long b, unsigned long e) {}
 static inline int swsusp_page_is_forbidden(struct page *p) { return 0; }
 static inline void swsusp_set_page_free(struct page *p) {}
 static inline void swsusp_unset_page_free(struct page *p) {}
+
+static inline int hibernate_register_nosave_region(unsigned long start_pfn, unsigned long end_pfn){
+	return 0;
+}
+static inline int hibernate_remove_nosave_region(unsigned long start_pfn, unsigned long end_pfn){
+	return 0;
+}
 
 static inline void hibernation_set_ops(const struct platform_hibernation_ops *ops) {}
 static inline int hibernate(void) { return -ENOSYS; }
