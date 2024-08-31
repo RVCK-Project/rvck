6a77046601445e63e6e6605c6273745b4bbcab7c
Han Gao <gaohan@iscas.ac.cn>
2024-09-01 00:43:06 +0800

    RISC-V laptop: Add new drivers for specific hardware components
    This commit adds various new drivers to support specific hardware components in RISC-V based laptops. These additions cover multiple subsystems including input, misc, net, and video.
    
    Details:
    - sound/
    - drivers/input/fingerprint: Add support for fingerprint sensors.
    - drivers/input/hall: Add support for hall sensors.
    - drivers/misc/hc32fx: Add HC32Fx series microcontroller support.
    - drivers/misc/usb_en.c: Add USB enable functionality.
    - drivers/net/wireless/rtl8822cs: Add Realtek RTL8822CS Wi-Fi driver.
    - drivers/video/lt8911exb: Add LT8911EXB video bridge driver.
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>
    
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

---
+++ drivers/misc/hc32fx/hc32fx_mcu.h
@@ -0,0 +1,14 @@
+#ifndef _HC32Fx_H_
+#define _HC32Fx_H_
+
+#define HC32FX_MCUVERSION_00         0x00
+#define HC32FX_MCUSTATUS_10          0x10
+#define HC32FX_SYSPOWERCONTROL_20    0x20
+#define HC32FX_MCUUPDATESTATUS_E0    0xE0
+#define HC32FX_BOOTLOADERMODE_F1     0xF1
+#define HC32FX_MAXREG_FF             0xFF
+
+// MCU COMMAND
+#define POWER_OFF   0x55
+#define BOOTLOADER  0x1F
+#endif
