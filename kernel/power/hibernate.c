cd8186d7c337d40810a324b41d9e68aa95bc3141
xianbing Zhu <xianbing.zhu@linux.alibaba.com>
2024-09-01 01:26:46 +0800

    hibernate: load image retry in some crc mismatch
    Firstly,load image again if occur crc mismatch.
    And if twice load image error, reboot retry for
    this boot.
    
    Signed-off-by: xianbing Zhu <xianbing.zhu@linux.alibaba.com>
    Signed-off-by: Han Gao <gaohan@iscas.ac.cn>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- kernel/power/hibernate.c
+++ kernel/power/hibernate.c
@@ -764,10 +764,20 @@ static int load_image_and_restore(bool snapshot_test)
 	}
 
 	error = swsusp_read(&flags);
-	swsusp_close(snapshot_test);
-	if (!error)
-		error = hibernation_restore(flags & SF_PLATFORM_MODE);
-
+	if (error == -ENODATA) { //if is crc error of image ,reboot retry
+		if(0 == swsusp_mark_sign_retry()) { //can retry
+			pr_warn("WARN:Image load error,reboot retry...\n");
+			swsusp_close(snapshot_test);
+			kernel_restart(NULL);
+		}
+		else //reach the retry max times
+			swsusp_close(snapshot_test);
+	}
+	else {
+		swsusp_close(snapshot_test);
+		if (!error)
+			error = hibernation_restore(flags & SF_PLATFORM_MODE);
+	}
 	pr_err("Failed to load image, recovering.\n");
 	swsusp_free();
 	free_basic_memory_bitmaps();
