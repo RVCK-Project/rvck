8d303dd61926a2b194e0365f2bc91e8dfde9cc94
haijiao.liu <haijiao.liu@sophgo.com>
2025-04-22 12:08:20 +0800

    drivers: i2c: Add ACPI support for Sophgo I2C Controller
    community inclusion
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/47
    
    --------------------------------
    
    Add SOPH0003 to the ACPI APD support list to ensure correct clock
    settings for the I2C devices on the Sophgo SG2044 platforms.
    
    Signed-off-by: haijiao.liu <haijiao.liu@sophgo.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- drivers/acpi/acpi_apd.c
+++ drivers/acpi/acpi_apd.c
@@ -40,7 +40,7 @@ struct apd_private_data {
 	const struct apd_device_desc *dev_desc;
 };
 
-#if defined(CONFIG_X86_AMD_PLATFORM_DEVICE) || defined(CONFIG_ARM64)
+#if defined(CONFIG_X86_AMD_PLATFORM_DEVICE) || defined(CONFIG_ARM64) || defined(CONFIG_RISCV)
 #define APD_ADDR(desc)	((unsigned long)&desc)
 
 static int acpi_apd_setup(struct apd_private_data *pdata)
@@ -178,6 +178,13 @@ static const struct apd_device_desc hip08_spi_desc = {
 };
 #endif /* CONFIG_ARM64 */
 
+#ifdef CONFIG_RISCV
+static const struct apd_device_desc sophgo_i2c_desc = {
+	.setup = acpi_apd_setup,
+	.fixed_clk_rate = 100000000,
+};
+#endif
+
 #endif
 
 /*
@@ -246,6 +253,9 @@ static const struct acpi_device_id acpi_apd_device_ids[] = {
 	{ "HISI02A3", APD_ADDR(hip08_lite_i2c_desc) },
 	{ "HISI0173", APD_ADDR(hip08_spi_desc) },
 	{ "NXP0001", APD_ADDR(nxp_i2c_desc) },
+#endif
+#ifdef CONFIG_RISCV
+	{ "SOPH0003", APD_ADDR(sophgo_i2c_desc) },
 #endif
 	{ }
 };
