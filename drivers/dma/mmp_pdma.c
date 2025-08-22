6b04aebbb88a76b2657a0fcea188bb7c2bfe9b07
Guodong Xu <guodong@riscstar.com>
2025-08-22 11:06:29 +0800

    dmaengine: mmp_pdma: Add reset controller support
    mainline inclusion
    from mainline-7.2-rc1
    commit fc72462bc610
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/320
    
    Replace devm_reset_control_get_optional_exclusive_deasserted() with explicit
    devm_reset_control_get_optional_exclusive() and reset_control_deassert() calls
    for compatibility with Linux 6.6 kernels.
    
    --------------------------------
    
    Add support to acquire and deassert an optional hardware reset controller
    during mmp_pdma_probe(). It is optional because in Marvell devices
    such as "marvell,pdma-1.0" the resets property is not a required
    property. But in SpacemiT K1 PDMA, "spacemit,k1-pdma" as the dt
    binding schema file stated, resets is required.
    
    Signed-off-by: Guodong Xu <guodong@riscstar.com>
    Link: https://lore.kernel.org/r/20250822-working_dma_0701_v2-v5-3-f5c0eda734cc@riscstar.com
    Signed-off-by: Vinod Koul <vkoul@kernel.org>
    Signed-off-by: Yongchao Jia <yongchao.jia@spacemit.com>

--- drivers/dma/mmp_pdma.c
+++ drivers/dma/mmp_pdma.c
@@ -16,6 +16,7 @@
 #include <linux/platform_data/mmp_dma.h>
 #include <linux/dmapool.h>
 #include <linux/clk.h>
+#include <linux/reset.h>
 #include <linux/of_dma.h>
 #include <linux/of.h>
 
@@ -1027,6 +1028,7 @@ static int mmp_pdma_probe(struct platform_device *op)
 	struct mmp_pdma_device *pdev;
 	struct mmp_dma_platdata *pdata = dev_get_platdata(&op->dev);
 	struct clk *clk;
+	struct reset_control *rst;
 	int i, ret, irq = 0;
 	int dma_channels = 0, irq_num = 0;
 	const enum dma_slave_buswidth widths =
@@ -1048,6 +1050,14 @@ static int mmp_pdma_probe(struct platform_device *op)
 	clk = devm_clk_get_optional_enabled(pdev->dev, NULL);
 	if (IS_ERR(clk))
 		return PTR_ERR(clk);
+
+	rst = devm_reset_control_get_optional_exclusive(pdev->dev, NULL);
+	if (IS_ERR(rst))
+		return PTR_ERR(rst);
+
+	ret = reset_control_deassert(rst);
+	if (ret)
+		return ret;
 
 	if (pdev->dev->of_node) {
 		/* Parse new and deprecated dma-channels properties */
