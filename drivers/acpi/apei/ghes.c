79298562d7dedbf17d4349d6d6fcbef645b025a8
Himanshu Chauhan <hchauhan@ventanamicro.com>
2025-08-27 09:57:35 +0800

    riscv: conditionally compile GHES NMI spool function
    community inclusion
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/95
    Link: https://lore.kernel.org/all/20250227123628.2931490-6-hchauhan@ventanamicro.com/
    
    ----------------
    
    Compile ghes_in_nmi_spool_from_list only when NMI and SEA
    is enabled. Otherwise compilation fails with "defined but
    not used" error.
    
    Signed-off-by: Himanshu Chauhan <hchauhan@ventanamicro.com>
    Signed-off-by: Lu Peng <lu.peng3@zte.com.cn>
    Signed-off-by: liuqingtao <liu.qingtao2@zte.com.cn>

--- drivers/acpi/apei/ghes.c
+++ drivers/acpi/apei/ghes.c
@@ -1160,6 +1160,7 @@ no_work:
 	return rc;
 }
 
+#if defined(CONFIG_HAVE_ACPI_APEI_NMI) || defined(CONFIG_ACPI_APEI_SEA)
 static int ghes_in_nmi_spool_from_list(struct list_head *rcu_list,
 				       enum fixed_addresses fixmap_idx)
 {
@@ -1178,6 +1179,7 @@ static int ghes_in_nmi_spool_from_list(struct list_head *rcu_list,
 
 	return ret;
 }
+#endif
 
 #ifdef CONFIG_ACPI_APEI_SEA
 static LIST_HEAD(ghes_sea);
