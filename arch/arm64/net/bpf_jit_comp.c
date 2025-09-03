5b3b0cae73d09247ca1e791c1487bb4ebd48b781
chenlei <chenlei.oerv@isrc.iscas.ac.cn>
2025-09-03 18:07:00 +0800

    bpf: Adjust argument names of arch_prepare_bpf_trampoline()
    mainline inclusion
    from mainline-v6.8-rc1
    commit 7a3d9a159b178e87306a6e989071ed9a114a1a31
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/113
    Reference: https://lore.kernel.org/r/20231206224054.492250-3-song@kernel.org
    
    --------------------------------
    
    bpf: Adjust argument names of arch_prepare_bpf_trampoline()
    
    We are using "im" for "struct bpf_tramp_image" and "tr" for "struct
    bpf_trampoline" in most of the code base. The only exception is the
    prototype and fallback version of arch_prepare_bpf_trampoline(). Update
    them to match the rest of the code base.
    
    We mix "orig_call" and "func_addr" for the argument in different versions
    of arch_prepare_bpf_trampoline(). s/orig_call/func_addr/g so they match.
    
    Signed-off-by: Song Liu <song@kernel.org>
    Acked-by: Ilya Leoshkevich <iii@linux.ibm.com>
    Tested-by: Ilya Leoshkevich <iii@linux.ibm.com>  # on s390x
    Acked-by: Jiri Olsa <jolsa@kernel.org>
    Link: https://lore.kernel.org/r/20231206224054.492250-3-song@kernel.org
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: chenlei <chenlei.oerv@isrc.iscas.ac.cn>

--- arch/arm64/net/bpf_jit_comp.c
+++ arch/arm64/net/bpf_jit_comp.c
@@ -1883,7 +1883,7 @@ static bool is_struct_ops_tramp(const struct bpf_tramp_links *fentry_links)
  *
  */
 static int prepare_trampoline(struct jit_ctx *ctx, struct bpf_tramp_image *im,
-			      struct bpf_tramp_links *tlinks, void *orig_call,
+			      struct bpf_tramp_links *tlinks, void *func_addr,
 			      int nregs, u32 flags)
 {
 	int i;
@@ -1985,7 +1985,7 @@ static int prepare_trampoline(struct jit_ctx *ctx, struct bpf_tramp_image *im,
 
 	if (flags & BPF_TRAMP_F_IP_ARG) {
 		/* save ip address of the traced function */
-		emit_addr_mov_i64(A64_R(10), (const u64)orig_call, ctx);
+		emit_addr_mov_i64(A64_R(10), (const u64)func_addr, ctx);
 		emit(A64_STR64I(A64_R(10), A64_SP, ip_off), ctx);
 	}
 
@@ -2101,7 +2101,7 @@ static int prepare_trampoline(struct jit_ctx *ctx, struct bpf_tramp_image *im,
 int arch_prepare_bpf_trampoline(struct bpf_tramp_image *im, void *image,
 				void *image_end, const struct btf_func_model *m,
 				u32 flags, struct bpf_tramp_links *tlinks,
-				void *orig_call)
+				void *func_addr)
 {
 	int i, ret;
 	int nregs = m->nr_args;
@@ -2122,7 +2122,7 @@ int arch_prepare_bpf_trampoline(struct bpf_tramp_image *im, void *image,
 	if (nregs > 8)
 		return -ENOTSUPP;
 
-	ret = prepare_trampoline(&ctx, im, tlinks, orig_call, nregs, flags);
+	ret = prepare_trampoline(&ctx, im, tlinks, func_addr, nregs, flags);
 	if (ret < 0)
 		return ret;
 
@@ -2133,7 +2133,7 @@ int arch_prepare_bpf_trampoline(struct bpf_tramp_image *im, void *image,
 	ctx.idx = 0;
 
 	jit_fill_hole(image, (unsigned int)(image_end - image));
-	ret = prepare_trampoline(&ctx, im, tlinks, orig_call, nregs, flags);
+	ret = prepare_trampoline(&ctx, im, tlinks, func_addr, nregs, flags);
 
 	if (ret > 0 && validate_code(&ctx) < 0)
 		ret = -EINVAL;
