a6d2ab9a416d777134ce62b0cbc98deae450ec34
chenlei <chenlei.oerv@isrc.iscas.ac.cn>
2025-09-04 10:43:06 +0800

    selftests/bpf: Test PTR_MAYBE_NULL arguments of struct_ops operators.
    mainline inclusion
    from mainline-v6.9-rc1
    commit 00f239eccf461a6403b3c16e767d04f3954cae98
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/113
    Reference: https://lore.kernel.org/r/20240209023750.1153905-5-thinker.li@gmail.com
    
    --------------------------------
    
    selftests/bpf: Test PTR_MAYBE_NULL arguments of struct_ops operators.
    
    Test if the verifier verifies nullable pointer arguments correctly for BPF
    struct_ops programs.
    
    "test_maybe_null" in struct bpf_testmod_ops is the operator defined for the
    test cases here.
    
    A BPF program should check a pointer for NULL beforehand to access the
    value pointed by the nullable pointer arguments, or the verifier should
    reject the programs. The test here includes two parts; the programs
    checking pointers properly and the programs not checking pointers
    beforehand. The test checks if the verifier accepts the programs checking
    properly and rejects the programs not checking at all.
    
    Signed-off-by: Kui-Feng Lee <thinker.li@gmail.com>
    Link: https://lore.kernel.org/r/20240209023750.1153905-5-thinker.li@gmail.com
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Signed-off-by: chenlei <chenlei.oerv@isrc.iscas.ac.cn>

--- tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.c
+++ tools/testing/selftests/bpf/bpf_testmod/bpf_testmod.c
@@ -549,9 +549,12 @@ static const struct bpf_verifier_ops bpf_testmod_verifier_ops = {
 static int bpf_dummy_reg(void *kdata)
 {
 	struct bpf_testmod_ops *ops = kdata;
-	int r;
 
-	r = ops->test_2(4, 3);
+	/* Some test cases (ex. struct_ops_maybe_null) may not have test_2
+	 * initialized, so we need to check for NULL.
+	 */
+	if (ops->test_2)
+		ops->test_2(4, 3);
 
 	return 0;
 }
@@ -570,9 +573,16 @@ static int bpf_testmod_test_2(int a, int b)
 	return 0;
 }
 
+static int bpf_testmod_ops__test_maybe_null(int dummy,
+					    struct task_struct *task__nullable)
+{
+	return 0;
+}
+
 static struct bpf_testmod_ops __bpf_testmod_ops = {
 	.test_1 = bpf_testmod_test_1,
 	.test_2 = bpf_testmod_test_2,
+	.test_maybe_null = bpf_testmod_ops__test_maybe_null,
 };
 
 struct bpf_struct_ops bpf_bpf_testmod_ops = {
