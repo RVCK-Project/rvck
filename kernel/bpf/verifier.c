0710bd4602d8c68eabb639fb47747599334dfcbe
chenlei <chenlei.oerv@isrc.iscas.ac.cn>
2025-09-04 10:51:34 +0800

    bpf: Recognize '__map' suffix in kfunc arguments
    mainline inclusion
    from mainline-v6.9-rc1
    commit 8d94f1357c00d7706c1f3d0bb568e054cef6aea1
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/113
    Reference: https://lore.kernel.org/r/20240307031228.42896-3-alexei.starovoitov@gmail.com
    
    --------------------------------
    
    bpf: Recognize '__map' suffix in kfunc arguments
    
    Recognize 'void *p__map' kfunc argument as 'struct bpf_map *p__map'.
    It allows kfunc to have 'void *' argument for maps, since bpf progs
    will call them as:
    struct {
            __uint(type, BPF_MAP_TYPE_ARENA);
    	...
    } arena SEC(".maps");
    
    bpf_kfunc_with_map(... &arena ...);
    
    Underneath libbpf will load CONST_PTR_TO_MAP into the register via ld_imm64
    insn. If kfunc was defined with 'struct bpf_map *' it would pass the
    verifier as well, but bpf prog would need to type cast the argument
    (void *)&arena, which is not clean.
    
    Acked-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/r/20240307031228.42896-3-alexei.starovoitov@gmail.com
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>
    Signed-off-by: chenlei <chenlei.oerv@isrc.iscas.ac.cn>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -10677,6 +10677,11 @@ static bool is_kfunc_arg_ignore(const struct btf *btf, const struct btf_param *a
 	return btf_param_match_suffix(btf, arg, "__ign");
 }
 
+static bool is_kfunc_arg_map(const struct btf *btf, const struct btf_param *arg)
+{
+	return btf_param_match_suffix(btf, arg, "__map");
+}
+
 static bool is_kfunc_arg_alloc_obj(const struct btf *btf, const struct btf_param *arg)
 {
 	return btf_param_match_suffix(btf, arg, "__alloc");
@@ -10846,6 +10851,7 @@ enum kfunc_ptr_arg_type {
 	KF_ARG_PTR_TO_RB_NODE,
 	KF_ARG_PTR_TO_NULL,
 	KF_ARG_PTR_TO_CONST_STR,
+	KF_ARG_PTR_TO_MAP,
 };
 
 enum special_kfunc_type {
@@ -10992,6 +10998,9 @@ get_kfunc_ptr_arg_type(struct bpf_verifier_env *env,
 
 	if (is_kfunc_arg_const_str(meta->btf, &args[argno]))
 		return KF_ARG_PTR_TO_CONST_STR;
+
+	if (is_kfunc_arg_map(meta->btf, &args[argno]))
+		return KF_ARG_PTR_TO_MAP;
 
 	if ((base_type(reg->type) == PTR_TO_BTF_ID || reg2btf_ids[base_type(reg->type)])) {
 		if (!btf_type_is_struct(ref_t)) {
@@ -11585,6 +11594,7 @@ static int check_kfunc_args(struct bpf_verifier_env *env, struct bpf_kfunc_call_
 		switch (kf_arg_type) {
 		case KF_ARG_PTR_TO_NULL:
 			continue;
+		case KF_ARG_PTR_TO_MAP:
 		case KF_ARG_PTR_TO_ALLOC_BTF_ID:
 		case KF_ARG_PTR_TO_BTF_ID:
 			if (!is_kfunc_trusted_args(meta) && !is_kfunc_rcu(meta))
@@ -11792,6 +11802,12 @@ static int check_kfunc_args(struct bpf_verifier_env *env, struct bpf_kfunc_call_
 			if (ret < 0)
 				return ret;
 			break;
+		case KF_ARG_PTR_TO_MAP:
+			/* If argument has '__map' suffix expect 'struct bpf_map *' */
+			ref_id = *reg2btf_ids[CONST_PTR_TO_MAP];
+			ref_t = btf_type_by_id(btf_vmlinux, ref_id);
+			ref_tname = btf_name_by_offset(btf, ref_t->name_off);
+			fallthrough;
 		case KF_ARG_PTR_TO_BTF_ID:
 			/* Only base_type is checked, further checks are done here */
 			if ((base_type(reg->type) != PTR_TO_BTF_ID ||
