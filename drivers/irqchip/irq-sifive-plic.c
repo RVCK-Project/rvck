04e796551ec5652afec2fae47ef29d276dc5515d
Jia Wang <wangjia@ultrarisc.com>
2025-09-18 10:44:01 +0800

    riscv: dp1000: plic: add plic early init supports
    community inclusion
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/71
    
    -------------------------------------------------
    
    Add PLIC early init supports and remove invalid
    timer nodes in dp1000.dts.
    
    Signed-off-by: Jia Wang <wangjia@ultrarisc.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- drivers/irqchip/irq-sifive-plic.c
+++ drivers/irqchip/irq-sifive-plic.c
@@ -825,3 +825,4 @@ static int __init plic_early_probe(struct device_node *node,
 
 IRQCHIP_DECLARE(thead_c900_plic, "thead,c900-plic", plic_early_probe);
 IRQCHIP_DECLARE(riscv, "allwinner,sun20i-d1-plic", plic_early_probe);
+IRQCHIP_DECLARE(ultrarisc_dp1000_plic, "ultrarisc,dp1000-plic", plic_early_probe);
