6326e4b86f821926c1046d69bd78e0de1ffd3534
gaorui <gao.rui@zte.com.cn>
2025-09-25 10:55:07 +0800

    iommu: Resolve fwspec ops automatically
    mainline inclusion
    from mainline-v6.10-rc7
    commit 3f7c320916282c26812d70cfe8830abb9e4dc696
    category: bugfix
    bugzilla: https://github.com/RVCK-Project/rvck/issues/142
    
    --------------------------------
    
    There's no real need for callers to resolve ops from a fwnode in order
    to then pass both to iommu_fwspec_init() - it's simpler and more sensible
    for that to resolve the ops itself. This in turn means we can centralise
    the notion of checking for a present driver, and enforce that fwspecs
    aren't allocated unless and until we know they will be usable.
    
    Also use this opportunity to modernise with some "new" helpers that
    arrived shortly after this code was first written; the generic
    fwnode_handle_get() clears up that ugly get/put mismatch, while
    of_fwnode_handle() can now abstract those open-coded dereferences.
    
    Tested-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/0e2727adeb8cd73274425322f2f793561bdc927e.1719919669.git.robin.murphy@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: gaorui <gao.rui@zte.com.cn>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -702,11 +702,9 @@ struct iommu_sva {
 	struct iommu_domain		*domain;
 };
 
-int iommu_fwspec_init(struct device *dev, struct fwnode_handle *iommu_fwnode,
-		      const struct iommu_ops *ops);
+int iommu_fwspec_init(struct device *dev, struct fwnode_handle *iommu_fwnode);
 void iommu_fwspec_free(struct device *dev);
 int iommu_fwspec_add_ids(struct device *dev, const u32 *ids, int num_ids);
-const struct iommu_ops *iommu_ops_from_fwnode(struct fwnode_handle *fwnode);
 
 static inline struct iommu_fwspec *dev_iommu_fwspec_get(struct device *dev)
 {
@@ -1025,8 +1023,7 @@ static inline void iommu_device_unlink(struct device *dev, struct device *link)
 }
 
 static inline int iommu_fwspec_init(struct device *dev,
-				    struct fwnode_handle *iommu_fwnode,
-				    const struct iommu_ops *ops)
+				    struct fwnode_handle *iommu_fwnode)
 {
 	return -ENODEV;
 }
@@ -1039,12 +1036,6 @@ static inline int iommu_fwspec_add_ids(struct device *dev, u32 *ids,
 				       int num_ids)
 {
 	return -ENODEV;
-}
-
-static inline
-const struct iommu_ops *iommu_ops_from_fwnode(struct fwnode_handle *fwnode)
-{
-	return NULL;
 }
 
 static inline int
