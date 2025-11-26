f79f82fcc63d58cf84996a91325986e180305e0d
Zhang Meng <zhangmeng.kevin@spacemit.com>
2025-11-26 09:48:14 +0800

    driver: clk: use the mainline clock and reset driver for spacemit k1
    community inclusion
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/155
    
    --------------------------------
    
    The clock and reset driver has been supported by linux 6.17, and the
    driver has been back-porting to linux rvck.
    
    Signed-off-by: Zhang Meng <zhangmeng.kevin@spacemit.com>
    Signed-off-by: Yanteng Si <si.yanteng@linux.dev>

--- drivers/clk/spacemit/ccu_pll.h
+++ drivers/clk/spacemit/ccu_pll.h
@@ -15,15 +15,25 @@
  * struct ccu_pll_rate_tbl - Structure mapping between PLL rate and register
  * configuration.
  *
+ * Usual PLL type
  * @rate:	PLL rate
  * @swcr1:	Register value of PLLX_SW1_CTRL (PLLx_SWCR1).
+ * @swcr2:	Not use.
  * @swcr3:	Register value of the PLLx_SW3_CTRL's lowest 31 bits of
  *		PLLx_SW3_CTRL (PLLx_SWCR3). This highest bit is for enabling
  *		the PLL and not contained in this field.
+ *
+ * Special PLL type A
+ * @rate:	PLLA rate
+ * @swcr1:	Register value of PLLAX_SW1_CTRL (PLLAx_SWCR1).
+ * @swcr2:	Register value of PLLAX_SW2_CTRL[15:8] (PLLAx_SWCR2). PLLAX_SW2_CTRL[16]
+ *		is for enabling the PLLA and not contained in this field.
+ * @swcr3:	Register value of PLLAX_SW3_CTRL (PLLAx_SWCR3).
  */
 struct ccu_pll_rate_tbl {
 	unsigned long rate;
 	u32 swcr1;
+	u32 swcr2;
 	u32 swcr3;
 };
 
@@ -36,8 +46,16 @@ struct ccu_pll_config {
 
 #define CCU_PLL_RATE(_rate, _swcr1, _swcr3) \
 	{									\
-		.rate	= _rate,							\
+		.rate	= _rate,						\
 		.swcr1	= _swcr1,						\
+		.swcr3	= _swcr3,						\
+	}
+
+#define CCU_PLLA_RATE(_rate, _swcr1, _swcr2, _swcr3) \
+	{									\
+		.rate	= _rate,						\
+		.swcr1	= _swcr1,						\
+		.swcr2	= _swcr2,						\
 		.swcr3	= _swcr3,						\
 	}
 
@@ -63,6 +81,15 @@ struct ccu_pll {
 		.flags		= _flags,					\
 	})
 
+#define CCU_PLLA_HWINIT(_name, _flags)						\
+	(&(struct clk_init_data) {						\
+		.name		= #_name,					\
+		.ops		= &spacemit_ccu_plla_ops,			\
+		.parent_data	= &(struct clk_parent_data) { .index = 0 },	\
+		.num_parents	= 1,						\
+		.flags		= _flags,					\
+	})
+
 #define CCU_PLL_DEFINE(_name, _table, _reg_swcr1, _reg_swcr3, _reg_lock,	\
 		       _mask_lock, _flags)					\
 static struct ccu_pll _name = {							\
@@ -74,6 +101,18 @@ static struct ccu_pll _name = {							\
 	}									\
 }
 
+#define CCU_PLLA_DEFINE(_name, _table, _reg_swcr1, _reg_swcr2, _reg_swcr3,	\
+		       _reg_lock, _mask_lock, _flags)				\
+static struct ccu_pll _name = {							\
+	.config	= CCU_PLL_CONFIG(_table, _reg_lock, _mask_lock),		\
+	.common = {								\
+		.reg_swcr1	= _reg_swcr1,					\
+		.reg_swcr2	= _reg_swcr2,					\
+		.reg_swcr3	= _reg_swcr3,					\
+		.hw.init	= CCU_PLLA_HWINIT(_name, _flags)		\
+	}									\
+}
+
 static inline struct ccu_pll *hw_to_ccu_pll(struct clk_hw *hw)
 {
 	struct ccu_common *common = hw_to_ccu_common(hw);
@@ -82,5 +121,6 @@ static inline struct ccu_pll *hw_to_ccu_pll(struct clk_hw *hw)
 }
 
 extern const struct clk_ops spacemit_ccu_pll_ops;
+extern const struct clk_ops spacemit_ccu_plla_ops;
 
 #endif
---
+++ drivers/reset/spacemit/reset-spacemit.c
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+/*
+ * Copyright (c) 2025 SpacemiT Technology Co. Ltd
+ * Copyright (c) 2025 Alex Elder <elder@riscstar.com>
+ */
+#include <linux/auxiliary_bus.h>
+#include <linux/container_of.h>
+#include <linux/device.h>
+#include <linux/module.h>
+#include <linux/regmap.h>
+#include <linux/reset-controller.h>
+#include <linux/types.h>
+#include <soc/spacemit/spacemit-syscon.h>
+#include "reset-spacemit.h"
+#include "reset-k1.h"
+
+static int spacemit_reset_update(struct reset_controller_dev *rcdev,
+				 unsigned long id, bool assert)
+{
+	struct ccu_reset_controller *controller;
+	const struct ccu_reset_data *data;
+	u32 mask;
+	u32 val;
+
+	controller = container_of(rcdev, struct ccu_reset_controller, rcdev);
+	data = &controller->data->reset_data[id];
+	mask = data->assert_mask | data->deassert_mask;
+	val = assert ? data->assert_mask : data->deassert_mask;
+
+	return regmap_update_bits(controller->regmap, data->offset, mask, val);
+}
+
+static int spacemit_reset_assert(struct reset_controller_dev *rcdev,
+				 unsigned long id)
+{
+	return spacemit_reset_update(rcdev, id, true);
+}
+
+static int spacemit_reset_deassert(struct reset_controller_dev *rcdev,
+				   unsigned long id)
+{
+	return spacemit_reset_update(rcdev, id, false);
+}
+
+static const struct reset_control_ops spacemit_reset_control_ops = {
+	.assert		= spacemit_reset_assert,
+	.deassert	= spacemit_reset_deassert,
+};
+
+static int spacemit_reset_controller_register(struct device *dev,
+					      struct ccu_reset_controller *controller)
+{
+	struct reset_controller_dev *rcdev = &controller->rcdev;
+
+	rcdev->ops = &spacemit_reset_control_ops;
+	rcdev->owner = THIS_MODULE;
+	rcdev->of_node = dev->of_node;
+	rcdev->nr_resets = controller->data->count;
+
+	return devm_reset_controller_register(dev, &controller->rcdev);
+}
+
+static int spacemit_reset_probe(struct auxiliary_device *adev,
+				const struct auxiliary_device_id *id)
+{
+	struct spacemit_ccu_adev *rdev = to_spacemit_ccu_adev(adev);
+	struct ccu_reset_controller *controller;
+	struct device *dev = &adev->dev;
+
+	controller = devm_kzalloc(dev, sizeof(*controller), GFP_KERNEL);
+	if (!controller)
+		return -ENOMEM;
+	controller->data = (const struct ccu_reset_controller_data *)id->driver_data;
+	controller->regmap = rdev->regmap;
+
+	return spacemit_reset_controller_register(dev, controller);
+}
+
+static const struct auxiliary_device_id spacemit_reset_ids[] = {
+#ifdef CONFIG_RESET_SPACEMIT_K1
+	K1_AUX_DEV_ID(mpmu),
+	K1_AUX_DEV_ID(apbc),
+	K1_AUX_DEV_ID(apmu),
+	K1_AUX_DEV_ID(rcpu),
+	K1_AUX_DEV_ID(rcpu2),
+	K1_AUX_DEV_ID(apbc2),
+#endif
+	{ },
+};
+MODULE_DEVICE_TABLE(auxiliary, spacemit_reset_ids);
+
+static struct auxiliary_driver spacemit_k1_reset_driver = {
+	.probe          = spacemit_reset_probe,
+	.id_table       = spacemit_reset_ids,
+};
+module_auxiliary_driver(spacemit_k1_reset_driver);
+
+MODULE_AUTHOR("Alex Elder <elder@kernel.org>");
+MODULE_DESCRIPTION("SpacemiT reset controller driver");
+MODULE_LICENSE("GPL");
