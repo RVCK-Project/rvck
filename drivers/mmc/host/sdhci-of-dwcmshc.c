d34c382f28c9a8766f03b1e3f77fc92c6f7a7412
ZhenXing Zhu <zhenxing.zhu@linux.alibaba.com>
2026-03-10 08:58:21 +0000

    Revert "chore: use xuantie instead of thead"
    dist inclusion
    category: cleanup
    Link: https://github.com/RVCK-Project/rvck/issues/227
    
    --------------------------------
    
    This reverts commit d6ab4d8e6ab22d55526843c383af86e564dd494a.
    
    The original commit renamed directories and references from "thead" to
    "xuantie". This change is being reverted because:
    
    - The renaming is part of a larger AON subsystem refactoring that
      depends on out-of-tree components
    - The upstream kernel still uses "thead" naming convention
    
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Signed-off-by: ZhenXing Zhu <zhenxing.zhu@linux.alibaba.com>

--- drivers/mmc/host/sdhci-of-dwcmshc.c
+++ drivers/mmc/host/sdhci-of-dwcmshc.c
@@ -1034,7 +1034,7 @@ static const struct of_device_id sdhci_dwcmshc_dt_ids[] = {
 		.data = &sdhci_dwcmshc_pdata,
 	},
 	{
-		.compatible = "xuantie,th1520-dwcmshc",
+		.compatible = "thead,th1520-dwcmshc",
 		.data = &sdhci_dwcmshc_th1520_pdata,
 	},
 	{},
