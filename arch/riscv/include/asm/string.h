cec9aa91624d3da6d3ebadac1b25ed87c7decc36
Feng Jiang <jiangfeng@kylinos.cn>
2026-04-03 19:28:47 -0600

    riscv: lib: add strnlen() implementation
    mainline inclusion
    from mainline-7.0-rc6
    commit 5ba15d419fab848a3813eb56bbcad00e291fbc49
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/280
    
    --------------------------------
    
    Add an optimized strnlen() implementation for RISC-V. This version
    includes a generic optimization and a Zbb-powered optimization using
    the 'orc.b' instruction, derived from the strlen() implementation.
    
    Benchmark results (QEMU TCG, rv64):
      Length | Original (MB/s) | Optimized (MB/s) | Improvement
      -------|-----------------|------------------|------------
      16 B   | 179             | 309              | +72.6%
      512 B  | 347             | 1562             | +350.1%
      4096 B | 356             | 1878             | +427.5%
    
    Suggested-by: Qingfang Deng <dqfext@gmail.com>
    Signed-off-by: Feng Jiang <jiangfeng@kylinos.cn>
    Link: https://patch.msgid.link/20260130025018.172925-7-jiangfeng@kylinos.cn
    Signed-off-by: Paul Walmsley <pjw@kernel.org>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>

--- arch/riscv/include/asm/string.h
+++ arch/riscv/include/asm/string.h
@@ -28,6 +28,9 @@ extern asmlinkage __kernel_size_t strlen(const char *);
 #define __HAVE_ARCH_STRNCMP
 extern asmlinkage int strncmp(const char *cs, const char *ct, size_t count);
 
+#define __HAVE_ARCH_STRNLEN
+extern asmlinkage __kernel_size_t strnlen(const char *, size_t);
+
 /* For those files which don't want to check by kasan. */
 #if defined(CONFIG_KASAN) && !defined(__SANITIZE_ADDRESS__)
 #define memcpy(dst, src, len) __memcpy(dst, src, len)
