246f19e5ba41bb850e6aa69d368a6d751edb7e7c
Zong Li <zong.li@sifive.com>
2026-04-04 18:40:58 -0600

    riscv: cfi: clear CFI lock status in start_thread()
    mainline inclusion
    from mainline-7.0
    commit a6ede084c4b7 ("riscv: cfi: clear CFI lock status in start_thread()")
    category: feature
    Link: https://github.com/RVCK-Project/rvck/issues/255
    
    --------------------------------
    
    When libc locks the CFI status through the following prctl:
     - PR_LOCK_SHADOW_STACK_STATUS
     - PR_LOCK_INDIR_BR_LP_STATUS
    
    A newly execd address space will inherit the lock status
    if it does not clear the lock bits. Since the lock bits
    remain set, libc will later fail to enable the landing
    pad and shadow stack.
    
    Signed-off-by: Zong Li <zong.li@sifive.com>
    Link: https://patch.msgid.link/20260323065640.4045713-1-zong.li@sifive.com
    [pjw@kernel.org: ensure we unlock before changing state; cleaned up subject line]
    Signed-off-by: Paul Walmsley <pjw@kernel.org>
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Signed-off-by: ZhenXing Zhu <zhenxing.zhu@linux.alibaba.com>

--- arch/riscv/kernel/process.c
+++ arch/riscv/kernel/process.c
@@ -151,6 +151,7 @@ void start_thread(struct pt_regs *regs, unsigned long pc,
 	 * clear shadow stack state on exec.
 	 * libc will set it later via prctl.
 	 */
+	set_shstk_lock(current, false);
 	set_shstk_status(current, false);
 	set_shstk_base(current, 0, 0);
 	set_active_shstk(current, 0);
@@ -158,6 +159,7 @@ void start_thread(struct pt_regs *regs, unsigned long pc,
 	 * disable indirect branch tracking on exec.
 	 * libc will enable it later via prctl.
 	 */
+	set_indir_lp_lock(current, false);
 	set_indir_lp_status(current, false);
 
 #ifdef CONFIG_64BIT
