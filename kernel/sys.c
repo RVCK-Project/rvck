42ea61b2895fdd458f846617e24360c1adf91e52
Paul Walmsley <pjw@kernel.org>
2026-04-04 18:40:58 -0600

    prctl: cfi: change the branch landing pad prctl()s to be more descriptive
    mainline inclusion
    from mainline-7.0
    commit 08ee1559052b ("prctl: cfi: change the branch landing pad prctl()s to be more descriptive")
    category: feature
    Link: https://github.com/RVCK-Project/rvck/issues/255
    
    --------------------------------
    
    Per Linus' comments requesting the replacement of "INDIR_BR_LP" in the
    indirect branch tracking prctl()s with something more readable, and
    suggesting the use of the speculation control prctl()s as an exemplar,
    reimplement the prctl()s and related constants that control per-task
    forward-edge control flow integrity.
    
    This primarily involves two changes.  First, the prctls are
    restructured to resemble the style of the speculative execution
    workaround control prctls PR_{GET,SET}_SPECULATION_CTRL, to make them
    easier to extend in the future.  Second, the "indir_br_lp" abbrevation
    is expanded to "branch_landing_pads" to be less telegraphic.  The
    kselftest and documentation is adjusted accordingly.
    
    Link: https://lore.kernel.org/linux-riscv/CAHk-=whhSLGZAx3N5jJpb4GLFDqH_QvS07D+6BnkPWmCEzTAgw@mail.gmail.com/
    Cc: Deepak Gupta <debug@rivosinc.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mark Brown <broonie@kernel.org>
    Signed-off-by: Paul Walmsley <pjw@kernel.org>
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Signed-off-by: ZhenXing Zhu <zhenxing.zhu@linux.alibaba.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2801,7 +2801,7 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 	case PR_GET_SHADOW_STACK_STATUS:
 		if (arg3 || arg4 || arg5)
 			return -EINVAL;
-		error = arch_get_shadow_stack_status(me, (unsigned long __user *) arg2);
+		error = arch_get_shadow_stack_status(me, (unsigned long __user *)arg2);
 		break;
 	case PR_SET_SHADOW_STACK_STATUS:
 		if (arg3 || arg4 || arg5)
@@ -2813,20 +2813,23 @@ SYSCALL_DEFINE5(prctl, int, option, unsigned long, arg2, unsigned long, arg3,
 			return -EINVAL;
 		error = arch_lock_shadow_stack_status(me, arg2);
 		break;
-	case PR_GET_INDIR_BR_LP_STATUS:
-		if (arg3 || arg4 || arg5)
+	case PR_GET_CFI:
+		if (arg2 != PR_CFI_BRANCH_LANDING_PADS)
 			return -EINVAL;
-		error = arch_prctl_get_branch_landing_pad_state(me, (unsigned long __user *)arg2);
+		if (arg4 || arg5)
+			return -EINVAL;
+		error = arch_prctl_get_branch_landing_pad_state(me, (unsigned long __user *)arg3);
 		break;
-	case PR_SET_INDIR_BR_LP_STATUS:
-		if (arg3 || arg4 || arg5)
+	case PR_SET_CFI:
+		if (arg2 != PR_CFI_BRANCH_LANDING_PADS)
 			return -EINVAL;
-		error = arch_prctl_set_branch_landing_pad_state(me, arg2);
-		break;
-	case PR_LOCK_INDIR_BR_LP_STATUS:
-		if (arg2 || arg3 || arg4 || arg5)
+		if (arg4 || arg5)
 			return -EINVAL;
-		error = arch_prctl_lock_branch_landing_pad_state(me);
+		error = arch_prctl_set_branch_landing_pad_state(me, arg3);
+		if (error)
+			break;
+		if (arg3 & PR_CFI_LOCK && !(arg3 & PR_CFI_DISABLE))
+			error = arch_prctl_lock_branch_landing_pad_state(me);
 		break;
 	default:
 		error = -EINVAL;
