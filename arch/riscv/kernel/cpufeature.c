7c7b6cbd786242fab01aa91c2d290671c816c674
Atish Patra <atishp@rivosinc.com>
2026-08-07 01:09:04 -0700

    RISC-V: Add Ssccfg/Smcdeleg ISA extension definition and parsing
    community inclusion
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/366
    Reference: https://github.com/torvalds/linux/commit/78bdadec8ac64b82b6c5950882a7c9ceb4d01ca7
    
    --------------------------------
    
    Smcdeleg extension allows the M-mode to delegate selected counters
    to S-mode so that it can access those counters and correpsonding
    hpmevent CSRs without M-mode.
    
    Ssccfg (‘Ss’ for Privileged architecture and Supervisor-level
    extension, ‘ccfg’ for Counter Configuration) provides access to
    delegated counters and new supervisor-level state.
    
    This patch just enables these definitions and enable parsing.
    
    Signed-off-by: Atish Patra <atishp@rivosinc.com>
    Link: https://patch.msgid.link/20260701-counter_delegation-v8-9-7909f863a645@meta.com
    Signed-off-by: Paul Walmsley <pjw@kernel.org>
    Reviewed-by: Charlie Jenkins <thecharlesjenkins@gmail.com>
    Signed-off-by: lvyating <lv.yating@zte.com.cn>
    Signed-off-by: heyilin <he.yilin@zte.com.cn>
    Signed-off-by: liuqingtao <liu.qingtao2@zte.com.cn>

--- arch/riscv/kernel/cpufeature.c
+++ arch/riscv/kernel/cpufeature.c
@@ -251,6 +251,27 @@ static const unsigned int riscv_a_exts[] = {
 	RISCV_ISA_EXT_ZALRSC,
 };
 
+static int riscv_ext_smcdeleg_validate(const struct riscv_isa_ext_data *data,
+				       const unsigned long *isa_bitmap)
+{
+	if (__riscv_isa_extension_available(isa_bitmap, RISCV_ISA_EXT_SSCSRIND) &&
+	    __riscv_isa_extension_available(isa_bitmap, RISCV_ISA_EXT_ZIHPM) &&
+	    __riscv_isa_extension_available(isa_bitmap, RISCV_ISA_EXT_ZICNTR))
+		return 0;
+
+	return -EPROBE_DEFER;
+}
+
+static int riscv_ext_ssccfg_validate(const struct riscv_isa_ext_data *data,
+				     const unsigned long *isa_bitmap)
+{
+	if (!riscv_ext_smcdeleg_validate(data, isa_bitmap) &&
+	    __riscv_isa_extension_available(isa_bitmap, RISCV_ISA_EXT_SMCDELEG))
+		return 0;
+
+	return -EPROBE_DEFER;
+}
+
 static const unsigned int riscv_zk_bundled_exts[] = {
 	RISCV_ISA_EXT_ZBKB,
 	RISCV_ISA_EXT_ZBKC,
@@ -509,12 +530,15 @@ const struct riscv_isa_ext_data riscv_isa_ext[] = {
 	__RISCV_ISA_EXT_BUNDLE(zvksg, riscv_zvksg_bundled_exts),
 	__RISCV_ISA_EXT_DATA(zvkt, RISCV_ISA_EXT_ZVKT),
 	__RISCV_ISA_EXT_DATA(smaia, RISCV_ISA_EXT_SMAIA),
+	__RISCV_ISA_EXT_DATA_VALIDATE(smcdeleg, RISCV_ISA_EXT_SMCDELEG,
+				      riscv_ext_smcdeleg_validate),
 	__RISCV_ISA_EXT_DATA(smcntrpmf, RISCV_ISA_EXT_SMCNTRPMF),
 	__RISCV_ISA_EXT_DATA(smcsrind, RISCV_ISA_EXT_SMCSRIND),
 	__RISCV_ISA_EXT_DATA(smmpm, RISCV_ISA_EXT_SMMPM),
 	__RISCV_ISA_EXT_SUPERSET(smnpm, RISCV_ISA_EXT_SMNPM, riscv_xlinuxenvcfg_exts),
 	__RISCV_ISA_EXT_DATA(smstateen, RISCV_ISA_EXT_SMSTATEEN),
 	__RISCV_ISA_EXT_DATA(ssaia, RISCV_ISA_EXT_SSAIA),
+	__RISCV_ISA_EXT_DATA_VALIDATE(ssccfg, RISCV_ISA_EXT_SSCCFG, riscv_ext_ssccfg_validate),
 	__RISCV_ISA_EXT_DATA(sscofpmf, RISCV_ISA_EXT_SSCOFPMF),
 	__RISCV_ISA_EXT_DATA(sscsrind, RISCV_ISA_EXT_SSCSRIND),
 	__RISCV_ISA_EXT_SUPERSET(ssnpm, RISCV_ISA_EXT_SSNPM, riscv_xlinuxenvcfg_exts),
