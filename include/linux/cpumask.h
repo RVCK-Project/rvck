79796a7d6d315b0e1fda2767b3e18d2006b65ce2
Zong Li <zong.li@sifive.com>
2026-08-23 19:20:29 -0700

    iommu/riscv: create a auxiliary device for HPM
    community inclusion
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck/issues/395
    Reference: https://lore.kernel.org/all/20260824022034.3916570-3-zong.li@sifive.com/
    
    -------------------------------------------------
    
    Create an auxiliary device for HPM when the IOMMU supports a
    hardware performance monitor.
    
    Tested-by: Chen Pei <cp0613@linux.alibaba.com>
    Tested-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Reviewed-by: Guo Ren <guoren@kernel.org>
    Suggested-by: Samuel Holland <samuel.holland@sifive.com>
    Signed-off-by: Zong Li <zong.li@sifive.com>
    Reviewed-by: Yicong Yang <yang.yicong@picoheart.com>
    Signed-off-by: liuqingtao <liu.qingtao2@zte.com.cn>
    Signed-off-by: zhangcheng <zhang.cheng35@zte.com.cn>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -204,29 +204,6 @@ unsigned int cpumask_first_and_and(const struct cpumask *srcp1,
 }
 
 /**
- * cpumask_any_and_but - pick a "random" cpu from *mask1 & *mask2, but not this one.
- * @mask1: the first input cpumask
- * @mask2: the second input cpumask
- * @cpu: the cpu to ignore
- *
- * Returns >= nr_cpu_ids if no cpus set.
- */
-static inline
-unsigned int cpumask_any_and_but(const struct cpumask *mask1,
-				 const struct cpumask *mask2,
-				 unsigned int cpu)
-{
-	unsigned int i;
-
-	cpumask_check(cpu);
-	i = cpumask_first_and(mask1, mask2);
-	if (i != cpu)
-		return i;
-
-	return cpumask_next_and(cpu, mask1, mask2);
-}
-
-/**
  * cpumask_last - get the last CPU in a cpumask
  * @srcp:	- the cpumask pointer
  *
@@ -309,6 +286,29 @@ unsigned int cpumask_next_and(int n, const struct cpumask *src1p,
 		cpumask_check(n);
 	return find_next_and_bit(cpumask_bits(src1p), cpumask_bits(src2p),
 		small_cpumask_bits, n + 1);
+}
+
+/**
+ * cpumask_any_and_but - pick a "random" cpu from *mask1 & *mask2, but not this one.
+ * @mask1: the first input cpumask
+ * @mask2: the second input cpumask
+ * @cpu: the cpu to ignore
+ *
+ * Returns >= nr_cpu_ids if no cpus set.
+ */
+static inline
+unsigned int cpumask_any_and_but(const struct cpumask *mask1,
+				 const struct cpumask *mask2,
+				 unsigned int cpu)
+{
+	unsigned int i;
+
+	cpumask_check(cpu);
+	i = cpumask_first_and(mask1, mask2);
+	if (i != cpu)
+		return i;
+
+	return cpumask_next_and(cpu, mask1, mask2);
 }
 
 /**
